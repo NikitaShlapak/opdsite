3c793e20f9a6bf0c981feab2498b937fd03af8e2
NikitaShlapak <80392030+NikitaShlapak@users.noreply.github.com>
2023-03-24 22:43:57 +0300

    Update env.py

--- SNO/SNO/env.py
+++ SNO/SNO/env.py
@@ -2,13 +2,13 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-2^s_8f-+m&v4882wlctonrwr*2f#mgfw!7h2em^8jf0^7f2psg'
+SECRET_KEY = '123'
 DEBUG = True
 
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'stusciiate@gmail.com'
-EMAIL_HOST_PASSWORD = 'rdeughmjkawljfms'
+EMAIL_HOST_PASSWORD = '123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
