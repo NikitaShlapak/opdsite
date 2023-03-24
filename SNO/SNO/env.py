510edef3bf9fdec7ed91a89b4585fda557052c48
NikitaShlapak <80392030+NikitaShlapak@users.noreply.github.com>
2023-03-24 23:21:19 +0300

    Update env.py
    data remove

--- SNO/SNO/env.py
+++ SNO/SNO/env.py
@@ -2,25 +2,25 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-2^s_8f-+m&v4882wlctonrwr*2f#mgfw!7h2em^8jf0^7f2psg'
+SECRET_KEY = '123'
 DEBUG = False
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'stusciiate@gmail.com'
-EMAIL_HOST_PASSWORD = 'rdeughmjkawljfms'
-EMAIL_PORT = 587
+EMAIL_HOST_USER = '123'
+EMAIL_HOST_PASSWORD = '123'
+EMAIL_PORT = 123
 EMAIL_USE_TLS = True
 
-ALLOWED_HOSTS = ['opd.iate.obninsk.ru', '10.0.0.34', '10.0.0.183', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['opd.iate.obninsk.ru','127.0.0.1']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'opd',
-        'USER': 'opd',
-        'PASSWORD': 'Tsm4EIMXgwr9VK',
-        'HOST': 'localhost',
+        'NAME': '123',
+        'USER': '123',
+        'PASSWORD': '123',
+        'HOST': '123',
         'PORT': '',
     }
 }
